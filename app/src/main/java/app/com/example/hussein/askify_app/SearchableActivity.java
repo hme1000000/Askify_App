89a9ecd2b4c18ce1c0c3e8106eaa6b618880832b
hme1000000 <hme.1000000@gmail.com>
2015-10-30 00:46:46 +0200

    adding navigation bars

--- app/src/main/java/app/com/example/hussein/askify_app/SearchableActivity.java
+++ app/src/main/java/app/com/example/hussein/askify_app/SearchableActivity.java
@@ -1,32 +1,65 @@
 package app.com.example.hussein.askify_app;
 
+import android.app.Dialog;
+import android.app.FragmentTransaction;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
 
+import com.astuetz.PagerSlidingTabStrip;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class SearchableActivity extends AppCompatActivity {
+public class SearchableActivity extends AppCompatActivity implements
+        ActionBar.TabListener {
 
     public static ArrayAdapter<String> myAdapter;
+    private ViewPager viewPager;
+    private TabsPagerAdapter mAdapter;
+    private ActionBar actionBar;
+    // Tab titles
+
+
+
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
+        // on tab selected
+        // show respected fragment view
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_searchable);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -40,6 +73,25 @@ public class SearchableActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        //actionBar = getSupportActionBar();
+        //mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+        //viewPager.setAdapter(mAdapter);
+        //actionBar.setHomeButtonEnabled(false);
+        //actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Adding Tabs
+        /*for (String tab_name : tabs) {
+            actionBar.addTab(actionBar.newTab().setText(tab_name)
+                    .setTabListener(this));
+        }*/
+
+        viewPager.setAdapter(new TabsPagerAdapter(getSupportFragmentManager()));
+
+        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        tabs.setViewPager(viewPager);
 
 
         List<String> myArray = new ArrayList<String>();
@@ -57,7 +109,30 @@ public class SearchableActivity extends AppCompatActivity {
         onSearchRequested();
         handleIntent(getIntent());
 
+        /*viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                // on changing the page
+                // make respected tab selected
+                actionBar.setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });*/
+
+
     }
+
+
+
+
 
 
 
@@ -67,6 +142,16 @@ public class SearchableActivity extends AppCompatActivity {
         onSearchRequested();
         super.onResume();
     }
+
+    @Override
+    protected void onPause() {
+        onSearchRequested();
+        super.onPause();
+    }
+
+
+
+
 
     @Override
     protected void onNewIntent(Intent intent) {
---
+++ app/src/main/java/app/com/example/hussein/askify_app/TabsPagerAdapter.java
@@ -0,0 +1,53 @@
+package app.com.example.hussein.askify_app;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by hussein on 29/10/15.
+ */
+public class TabsPagerAdapter extends FragmentStatePagerAdapter {
+
+    private String[] tabs = { "All", "Questions", "Answers","Users","Unsolved" };
+
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int index) {
+
+        switch (index) {
+            case 0:
+                // Top Rated fragment activity
+                return  new AllFragment();
+            case 1:
+                // Games fragment activity
+                return new QuestionFragment();
+            case 2:
+                // Movies fragment activity
+                return new AnswerFragment();
+            case 3:
+                // Movies fragment activity
+                return new UsersFragment();
+            case 4:
+                // Movies fragment activity
+                return new UnsolvedFragment();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // get item count - equal to number of tabs
+        return 5;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+}
